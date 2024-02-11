ae22639d4366d85de118821c1bafcf809a4527e5
Tushar Bendre <tusharbendre01@gmail.com>
2024-02-11 09:09:11 +0530

    Initial Commit

---
+++ Shop/ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopApp.swift
+//  Shop
+//
+//  Created by Tushar Bendre on 11/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
